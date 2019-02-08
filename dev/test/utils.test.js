4729e74b9e5e983d4e1dacb0c83195a807d88c36
Taras Labiak <kissarat@gmail.com>
2019-02-09 00:20:28 +0200

    + transformation in utilities

--- dev/test/utils.test.js
+++ dev/test/utils.test.js
@@ -1,10 +1,36 @@
-const {assert} = require("chai");
+const { assert } = require("chai");
 const projectRoot = __dirname + "/../..";
 const utils = require(projectRoot + "/utilities");
+
+const T = utils.transformation({
+  a: "e",
+  b: "f",
+  c: "g",
+  r: true,
+  f() {
+    return "z";
+  }
+});
 
 describe("utils", function () {
   it("load", function () {
     const string = utils.load(projectRoot + "/package.json#/name");
     assert("auxiliary" === string);
+  })
+
+  it("simple transformation", function () {
+    const input = {
+      a: 1,
+      b: 2,
+      c: 3,
+      r: 5,
+      f: 8
+    }
+    const { e, f, g, r, z } = T(input);
+    assert.strictEqual(e, input.a);
+    assert.strictEqual(f, input.b);
+    assert.strictEqual(g, input.c);
+    assert.strictEqual(r, input.r);
+    assert.strictEqual(z, input.f);
   })
 });
