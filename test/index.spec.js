eb5f164938cb8bf558a43660dc2fa9fdaea5d10b
Taras Labiak <kissarat@gmail.com>
2019-12-25 08:44:28 +0200

    + isObject

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,14 @@
-const { pick } = require('../lib');
+const { pick, isObject } = require('../lib');
 
 describe('index', () => {
+    it('isObject', () => {
+        for(const v of [null, true, '', ' ', 'abc', 0, 1, Infinity, NaN]) {
+            expect(isObject(true)).toBeFalse();
+        }
+        for(const v of [{}, {a: 'a'}, Object, class SomeObject {}]) {
+            expect(isObject(true)).toBeFalse();
+        }
+    });
     it('pick', () => {
         const source = { a: 1, b: 2 };
         const actual = pick(source, 'a');
