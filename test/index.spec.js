34f7eead4d872a208d2b64a6d14da3a0f05e3b35
Taras Labiak <kissarat@gmail.com>
2020-01-04 05:06:51 +0200

    + uniq

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const { pick, isObject } = require('../lib');
+const { pick, isObject, uniq } = require('../lib');
 
 describe('index', () => {
     it('isObject', () => {
@@ -9,10 +9,33 @@ describe('index', () => {
             expect(isObject(true)).toBeFalse();
         }
     });
+
     it('pick', () => {
         const source = { a: 1, b: 2 };
         const actual = pick(source, 'a');
         expect(actual.a).toEqual(source.a);
         expect(actual.b).toBeUndefined();
+    });
+
+    it('uniq number', () => {
+        const actual = [15, 250, 250, 101, 101, 1]
+        expect(uniq(actual)).toEqual([15, 250, 101, 1]);
+    });
+
+    it('uniq string', () => {
+        const actual = [
+            'fd96a283-900f-4d7e-8ef4-e518c560fbd9',
+            'fd96a283-900f-4d7e-8ef4-e518c560fbd9',
+            'e4b1628b-3220-4f4b-9290-5893b4229594',
+            'b3294a45-caf0-49be-9d37-ded515bad95b',
+            'b3294a45-caf0-49be-9d37-ded515bad95b',
+            '1bcfb557-feba-42c1-860b-c7cf7bb9a27c'
+        ]
+        expect(uniq(actual)).toEqual([
+            'fd96a283-900f-4d7e-8ef4-e518c560fbd9',
+            'e4b1628b-3220-4f4b-9290-5893b4229594',
+            'b3294a45-caf0-49be-9d37-ded515bad95b',
+            '1bcfb557-feba-42c1-860b-c7cf7bb9a27c'
+        ]);
     });
 });
