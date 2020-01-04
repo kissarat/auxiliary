92f768ae2af1b3425a9be1016cc66fc3e2483035
Taras Labiak <kissarat@gmail.com>
2020-01-04 02:21:42 +0200

    + conveyor set test

--- test/conveyor.spec.js
+++ test/conveyor.spec.js
@@ -23,10 +23,20 @@ describe('conveyor', () => {
         expect(actual.a).toEqual(10);
         expect(actual.b).toEqual(2);
     });
-    it('fiter', () => {
+    it('filter', () => {
         const actual = conveyor(['aa', 'ab', 'c'],
             filter(key => key[0] === 'a')
         );
         expect(actual).toEqual(['aa', 'ab']);
+    });
+    it('set', () => {
+        const object = { a: 'string', b: { type: 'number'}};
+        const actual = conveyor(object,
+            set(['a', 'b'], type => typeof type === 'string' ? { type } : type)
+        );
+        expect(actual).toEqual({
+            a: { type: 'string' },
+            b: { type: 'number'}
+        });
     });
 });
